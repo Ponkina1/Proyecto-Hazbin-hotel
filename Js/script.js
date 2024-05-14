afed7cadd11761bebe338516380057de2cb3a3b1
Ponkina1 <paulitas265@gmail.com>
2024-05-14 18:37:48 -0500

    Finish

---
+++ Js/script.js
@@ -0,0 +1,19 @@
+// Función para cambiar entre ciudades al hacer clic en las pestañas
+function openCity(evt, cityName) {
+  var i, x, tablinks;
+  x = document.getElementsByClassName("city");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" w3-border-red", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.firstElementChild.className += " w3-border-red";
+}
+
+function toggleNav() {
+  var sidenav = document.getElementById("sidenav");
+  sidenav.classList.toggle("visible"); // Agrega o quita la clase "visible"
+}
